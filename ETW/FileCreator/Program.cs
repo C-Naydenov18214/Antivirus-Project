06c993ffab0e14ee20fbe94b9b54d295914ff254
TuS <tussss89@gmail.com>
2021-05-19 23:00:45 +0700

    to f do

--- ETW/FileCreator/Program.cs
+++ ETW/FileCreator/Program.cs
@@ -4,17 +4,16 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
-namespace ReadAndWrite
+namespace FileCreator
 {
     class Program
     {
         static async Task Main(string[] args)
         {
             var curDir = Directory.GetCurrentDirectory();
-            var dirPath = $"{curDir}\\Files";
+            var dirPath = @"D:\Downloads\Studying\Antivirus-Project\ETW\ReadAndWrite\bin\Debug\Files";
             DirectoryInfo dir = new DirectoryInfo(dirPath);
             if (!dir.Exists)
             {
@@ -22,7 +21,7 @@ namespace ReadAndWrite
             }
             try
             {
-                /*for (int i = 0; i < 10; i++)
+                for (int i = 0; i < 10; i++)
                 {
                     string path = dir.FullName + @"\" + i.ToString() + ".txt";
                     Console.WriteLine(path);
@@ -30,28 +29,14 @@ namespace ReadAndWrite
                     {
                         await sw.WriteLineAsync($"This wrote { Process.GetCurrentProcess().Id} :)");
                     }
-                }*/
-                for (int i = 0; i < 10; i++)
-                {
-                    string path = dir.FullName + @"\" + i.ToString() + ".txt";
-                    using (StreamReader sr = new StreamReader(path))
-                    {
-                        var data = sr.ReadLine();
-                        string pathToWrite = dir.FullName + @"\" + (i + 100).ToString() + ".txt";
-                        using (StreamWriter sw = new StreamWriter(pathToWrite, false))
-                        {
-                            await sw.WriteLineAsync(data + " I did it again");
-                        }
-                        
-                    }
-                    File.Delete(path);
-                    Thread.Sleep(1000);
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
+
+
         }
     }
 }
--- ETW/ReadWriteDeleteAnalyzer/ReadWriteDeleteAnalyzer.cs
+++ ETW/ReadWriteDeleteAnalyzer/ReadWriteDeleteAnalyzer.cs
@@ -37,31 +37,30 @@ namespace ReadWriteDeleteAnalyzer
                 var read = pidGr.Where(el => el.Action.CompareTo("FileIO/Read") == 0).Publish().RefCount();
                 var write = pidGr.Where(el => el.Action.CompareTo("FileIO/Write") == 0).Publish().RefCount();
                 var delete = pidGr.Where(el => el.Action.CompareTo("FileIO/Delete") == 0).Publish().RefCount();
-                var firstPart = read.GroupJoin(write,
+                var firstPart = write.GroupJoin(read,
                     _ => Observable.Timer(TimeSpan.FromTicks(1)),
-                    _ => Observable.Never<Unit>().TakeUntil(read.LastOrDefaultAsync().CombineLatest(write.LastOrDefaultAsync())),
+                    _ => Observable.Never<Unit>().TakeUntil(write.LastOrDefaultAsync().CombineLatest(read.LastOrDefaultAsync())),
                     (w, r) => (w, r))
                 .SelectMany(x => x.r.Aggregate(new HashSet<int>(), (acc, v) => { acc.Add(v.PID); message = v.FName; return acc; }, acc => new
                 {
-                    FromName = x.w.FName,
-                    ToFile = message,
-                    procName = x.w.ProcName,
-                    writeBy = x.w.PID,
-                    creates = acc
-                })).Publish().RefCount();
+                    FName = x.w.FName,
+                    ProcName = x.w.ProcName,
+                    PID = x.w.PID,
+                    writes = acc
+                })).Where(x => x.writes.Contains(x.PID) && x.writes.Count == 1);
 
-                var res = delete.GroupJoin(firstPart,
+                /*var res = delete.GroupJoin(firstPart,
                     _ => Observable.Timer(TimeSpan.FromTicks(1)),
-                    _ => Observable.Never<Unit>().TakeUntil(delete.LastOrDefaultAsync().CombineLatest(firstPart.LastOrDefaultAsync())/*.Where(a => a.First.FName.CompareTo(a.Second.FromName) == 0)*/),
+                    _ => Observable.Never<Unit>().TakeUntil(delete.LastOrDefaultAsync().CombineLatest(firstPart.LastOrDefaultAsync())),
                     (d, r) => (d, r))
-                    .SelectMany(x => x.r.Aggregate(new HashSet<int>(), (acc, v) => { acc.Add(v.writeBy); message = v.FromName; return acc; }, acc => new
+                    .SelectMany(x => x.r.Aggregate(new HashSet<int>(), (acc, v) => { acc.Add(v.PID); message = v.FName; return acc; }, acc => new
                     {
                         FromName = $"\n\t read {message}-> write -> delete {x.d.FName}",
-                        procName = x.d.ProcName,
-                        writeBy = x.d.PID,
+                        ProcName = x.d.ProcName,
+                        PID = x.d.PID,
                         creates = acc
-                    })).Where(x => x.creates.Contains(x.writeBy) && x.creates.Count != 0); ;
-                return res;
+                    })).Where(x => x.creates.Contains(x.PID) && x.creates.Count != 0);*/
+                return firstPart;//res;
 
             }).Subscribe(e =>
             {
@@ -71,10 +70,10 @@ namespace ReadWriteDeleteAnalyzer
                 var r = new SuspiciousEvent();
                 try
                 {
-                    r.ProcessId = e.writeBy;
-                    r.EventInfo = e.FromName;
+                    r.ProcessId = e.PID;
+                    r.EventInfo = e.FName;
                     r.Length = r.EventInfo.Length;
-                    r.ProcName = e.procName;
+                    r.ProcName = e.ProcName;
                     this.SuspiciousEvents.OnNext(r);
                 }
                 catch (NullReferenceException ex)
