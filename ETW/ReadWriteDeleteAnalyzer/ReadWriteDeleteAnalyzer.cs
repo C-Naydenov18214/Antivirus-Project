b2c6aa506f8588ad00852ae3af1ec9f487bf9d01
TuS <tussss89@gmail.com>
2021-05-30 18:33:20 +0700

    upd

--- ETW/ReadWriteDeleteAnalyzer/ReadWriteDeleteAnalyzer.cs
+++ ETW/ReadWriteDeleteAnalyzer/ReadWriteDeleteAnalyzer.cs
@@ -27,9 +27,9 @@ namespace ReadWriteDeleteAnalyzer
         public override void Start()
         {
 
-            var streamW = _writes/*.Where(el => el.FileName.EndsWith(".txt"))*/.Select(el => new { PID = el.ProcessID, FName = el.FileName, Action = el.EventName, ProcName = el.ProcessName });
-            var streamR = _reads.Where(el => el.FileName.EndsWith(".txt")).Select(el => new { PID = el.ProcessID, FName = el.FileName, Action = el.EventName, ProcName = el.ProcessName });
-            var streamD = _deletes.Where(el => el.FileName.EndsWith(".txt")).Select(el => new { PID = el.ProcessID, FName = el.FileName, Action = el.EventName, ProcName = el.ProcessName });
+            var streamW = _writes.Where(el => (el.ProcessName.CompareTo("WinRAR") == 0 || el.ProcessName.CompareTo("ReadAndWrite") == 0))/*.Where(el => el.FileName.EndsWith(".txt"))*/.Select(el => new { PID = el.ProcessID, FName = el.FileName, Action = el.EventName, ProcName = el.ProcessName });
+            var streamR = _reads.Where(el => el.FileName.EndsWith(".txt") && (el.ProcessName.CompareTo("WinRAR") == 0 || el.ProcessName.CompareTo("ReadAndWrite") == 0)).Select(el => new { PID = el.ProcessID, FName = el.FileName, Action = el.EventName, ProcName = el.ProcessName });
+            var streamD = _deletes.Where(el => el.FileName.EndsWith(".txt") && (el.ProcessName.CompareTo("WinRAR") == 0 || el.ProcessName.CompareTo("ReadAndWrite") == 0)).Select(el => new { PID = el.ProcessID, FName = el.FileName, Action = el.EventName, ProcName = el.ProcessName });
 
 
             var byPid = streamR.Merge(streamW).Merge(streamD).GroupBy(el => el.PID);
@@ -67,7 +67,7 @@ namespace ReadWriteDeleteAnalyzer
 
                 var prdw = prd.GroupJoin(write,
                     _ => Observable.Timer(TimeSpan.FromTicks(1)),
-                    _ => Observable.Timer(TimeSpan.FromMilliseconds(100)),//Observable.Never<Unit>().TakeUntil(prd.LastOrDefaultAsync().CombineLatest(write.LastOrDefaultAsync())),
+                    _ => Observable.Timer(TimeSpan.FromMilliseconds(1000)),//Observable.Never<Unit>().TakeUntil(prd.LastOrDefaultAsync().CombineLatest(write.LastOrDefaultAsync())),
                     (rrd, w) => (rrd, w)).SelectMany(x => x.w.Aggregate(new HashSet<string>(), (acc, v) => { acc.Add(v.FName); return acc; }, acc => new
                     {
                         FDelete = x.rrd.FName,
@@ -77,39 +77,41 @@ namespace ReadWriteDeleteAnalyzer
                         FWrite = acc
 
                     }
-                      )).Where(x => x.FWrite.Count != 0).Select(x=> new {
+                      )).Where(x => x.FWrite.Count != 0).Select(x => new
+                      {
                           FDelete = x.FDelete,
                           PID = x.PID,
                           ProcName = x.ProcName,
-                          FRead = String.Join(", ",x.FRead.Select(i => i)),
-                          FWrite = String.Join(", ",x.FWrite.Select(i => i))
+                          FRead = String.Join(", ", x.FRead.Select(i => i)),
+                          FWrite = String.Join(", ", x.FWrite.Select(i => i))
                       });
 
 
-                /*.Where(x => x.writes.Count != 0)*/;//.GroupBy(x => x.FName).SelectMany(gr => gr.FirstOrDefaultAsync()).Publish().RefCount();
+                /*.Where(x => x.writes.Count != 0)*/
+                ;//.GroupBy(x => x.FName).SelectMany(gr => gr.FirstOrDefaultAsync()).Publish().RefCount();
                 return prdw;//prd;
 
             })
 
 
 
-                /*.Subscribe(e =>
+            /*.Subscribe(e =>
+            {
+                var r = new SuspiciousEvent();
+                try
                 {
-                    var r = new SuspiciousEvent();
-                    try
-                    {
-                        r.ProcessId = e.PID;
-                        r.EventInfo = $"\n\tread -> {e.FName} -> delete NO \n\t# caught writes: {String.Join(", ", e.writes.Select(i => i))}";
-                        //r.EventInfo = $"\n\tread -> {e.FName}";// -> delete {e.FDelet} \n\t# caught writes: {e.FWrite}";
-                        r.Length = r.EventInfo.Length;
-                        r.ProcName = e.ProcName;
-                        this.SuspiciousEvents.OnNext(r);
-                    }
-                    catch (NullReferenceException ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
-                });*/
+                    r.ProcessId = e.PID;
+                    r.EventInfo = $"\n\tread -> {e.FName} -> delete NO \n\t# caught writes: {String.Join(", ", e.writes.Select(i => i))}";
+                    //r.EventInfo = $"\n\tread -> {e.FName}";// -> delete {e.FDelet} \n\t# caught writes: {e.FWrite}";
+                    r.Length = r.EventInfo.Length;
+                    r.ProcName = e.ProcName;
+                    this.SuspiciousEvents.OnNext(r);
+                }
+                catch (NullReferenceException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            });*/
             .Subscribe(e =>
             {
                 var r = new SuspiciousEvent();
